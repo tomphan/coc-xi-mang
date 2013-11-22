fe08e5b26b6f5c72f4db4804bb457afa3b06babe
phale09 <nguyenthetu09@gmail.com>
2013-11-22 11:39:48 +0700

    ----------

---
+++ modules/digipro/vecoc/zoom.js
@@ -0,0 +1,28 @@
+
+jQuery(document).ready(function(){
+	var $section = $('#svgbasics1');
+	var $panzoom = $section.panzoom();
+	$panzoom.parent().on('mousewheel.focal', function( e ) {
+		e.preventDefault();
+		var delta = e.delta || e.originalEvent.wheelDelta;
+		var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+		$panzoom.panzoom('zoom', zoomOut, {
+			increment: 0.1,
+			focal: e
+		});
+	});
+});
+jQuery(document).ready(function(){
+	var $section = $('#svgbasics2');
+	var $panzoom = $section.panzoom();
+	$panzoom.parent().on('mousewheel.focal', function( e ) {
+		e.preventDefault();
+		var delta = e.delta || e.originalEvent.wheelDelta;
+		var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+		$panzoom.panzoom('zoom', zoomOut, {
+			increment: 0.1,
+			focal: e
+		});
+	});
+});
+
